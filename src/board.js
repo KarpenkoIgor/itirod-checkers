d576e2d0a42916cd652ddeacf9121273f01fdafe
KarpenkoIgor <hv.karpenko@gmail.com>
2022-05-18 23:08:02 +0300

    Added game logic

---
+++ src/board.js
@@ -0,0 +1,22 @@
+// var divSquare = '<div id="s$coord" class="square $color"></div>'
+
+// $(function (){
+//     addSquares()
+// });
+
+// function addSquares() {
+//     $('.board').html('');
+//     for (var coord = 0; coord < 64; coord++)
+//         $('.board').append(divSquare
+//             .replace('$color',
+//                 isBlackSquareAt(coord) ? 'black' : 'white'));
+// }
+
+// function isBlackSquareAt(coord) {
+//     return (coord % 8 + Math.floor(coord / 8)) % 2;
+// }
+document.body.innerHTML = "<h1>Today’s date is </h1>"
+document.body.innerHTML = "<h1>Today’s date is </h1>"
+document.body.innerHTML = "<h1>Today’s date is </h1>"
+document.body.innerHTML = "<h1>Today’s date is </h1>"
+document.body.innerHTML = "<h1>Today’s date is </h1>"
